4534d499f42fc8ed1fa297906fecb16d5959ab72
Basavanagowda Kanur <basavanagowda@gmail.com>
2026-03-28 18:56:42 +0530

    Run CLI specs via executable path

--- spec/cli/commands_spec.rb
+++ spec/cli/commands_spec.rb
@@ -5,11 +5,10 @@ require_relative 'spec_helper'
 
 RSpec.describe 'umgr commands', :cli do
   let(:executable) { File.expand_path('../../exe/umgr', __dir__) }
-  let(:lib_path) { File.expand_path('../../lib', __dir__) }
 
   %w[init validate plan apply show import].each do |command|
     it "dispatches #{command}" do
-      run_command("ruby -I #{lib_path} #{executable} #{command}")
+      run_command("#{executable} #{command}")
 
       expect(last_command_started).to have_exit_status(0)
       parsed = JSON.parse(last_command_started.stdout)
@@ -19,7 +18,7 @@ RSpec.describe 'umgr commands', :cli do
   end
 
   it 'passes config option to validate' do
-    run_command("ruby -I #{lib_path} #{executable} validate --config users.yml")
+    run_command("#{executable} validate --config users.yml")
 
     expect(last_command_started).to have_exit_status(0)
     parsed = JSON.parse(last_command_started.stdout)
--- spec/cli/help_spec.rb
+++ spec/cli/help_spec.rb
@@ -5,9 +5,8 @@ require_relative 'spec_helper'
 RSpec.describe 'umgr help', :cli do
   it 'shows available commands' do
     executable = File.expand_path('../../exe/umgr', __dir__)
-    lib_path = File.expand_path('../../lib', __dir__)
 
-    run_command("ruby -I #{lib_path} #{executable} help")
+    run_command("#{executable} help")
 
     expect(last_command_started).to have_output(/Commands:/)
     expect(last_command_started).to have_exit_status(0)
