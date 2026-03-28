edec1fbac944907e8efdc93ce56936e236b24155
Basavanagowda Kanur <basavanagowda@gmail.com>
2026-03-28 18:39:43 +0530

    Improve runner spec and CI workflow parallelism

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Umgr::Runner do
   it 'is instantiable' do
     expect(described_class.new).to be_a(described_class)
   end
+
+  it 'returns ok for #ping' do
+    expect(described_class.new.ping).to eq(:ok)
+  end
 end
