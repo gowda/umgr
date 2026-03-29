462b97d46d4cf95163ee3c580bd5382db8c0326d
Basavanagowda Kanur <basavanagowda@gmail.com>
2026-03-29 09:49:11 +0530

    Add SimpleCov reporting to checks workflow

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+
+SimpleCov.command_name(ENV.fetch('SIMPLECOV_COMMAND_NAME', 'rspec'))
+SimpleCov.start do
+  enable_coverage :branch
+  add_filter '/spec/'
+end
+
 require 'umgr'
 
 RSpec.configure do |config|
